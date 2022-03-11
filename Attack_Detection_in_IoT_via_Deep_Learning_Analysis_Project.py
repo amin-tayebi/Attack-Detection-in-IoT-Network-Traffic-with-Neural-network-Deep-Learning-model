218412576d030efadb7ba2b1ded0e86be540135e
Amin TAYEBI <amin.tayebi313@gmail.com>
2022-03-11 10:56:23 +0100

    Update Attack_Detection_in_IoT_via_Deep_Learning_Analysis_Project.py

--- Attack_Detection_in_IoT_via_Deep_Learning_Analysis_Project.py
+++ Attack_Detection_in_IoT_via_Deep_Learning_Analysis_Project.py
@@ -2,15 +2,32 @@
 #I put this just as a simple sample of a Neural Network Model, For having the main codes of the model we can have a collaboration#
 #                                                      please contact
 ##################################################################################################################################
-import numpy as np
+# Import required packages
 from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.datasets import boston_housing
-
+import math
+from keras.layers import Dense, Activation, BatchNormalization, Dropout
+from keras.utils import to_categorical
+# NumPy is a library for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays. 
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-
+import matplotlib.pyplot as plt
+import os
+# Keras is an open-source software library that provides a Python interface for artificial neural networks. 
+# Keras acts as an interface for the TensorFlow library. Up until version 2.3, Keras supported multiple backends, including TensorFlow, Microsoft Cognitive Toolkit, Theano, and PlaidML. 
+# contains numerous implementations of commonly used neural-network building blocks such as layers, objectives, activation functions, optimizers, and a host of tools to make working with image and text data easier to simplify the coding necessary for writing deep neural network code.
+from keras.callbacks import Callback
+# pandas is a software library written for the Python programming language for data manipulation and analysis. 
+# In particular, it offers data structures and operations for manipulating numerical tables and time series. 
+import pandas as pd
+# sklearn) is a free software machine learning library for the Python programming language.[3] It features various classification, regression and clustering algorithms including support vector machines, random forests, gradient boosting, k-means and DBSCAN, and is designed to interoperate with the Python numerical and scientific libraries NumPy and SciPy.
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import StratifiedKFold
+import matplotlib.pyplot as plt
+# Seaborn is an open-source Python library built on top of matplotlib. It is used for data visualization and exploratory data analysis. Seaborn works easily with dataframes and the Pandas library.
+import seaborn as sns
+from keras.optimizers import SGD
+from keras.callbacks import Callback,EarlyStopping
+import gzip
+#=======================================================================
 # Generate dummy training dataset --as 60% of dataset
 np.random.seed(2018)
 x_train = np.random.random((6000,10))
